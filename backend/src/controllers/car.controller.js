43d65f52df5666a9677af693729fe42c68c79b44
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-02-01 11:33:55 +0530

    Number field customized

--- backend/src/controllers/car.controller.js
+++ backend/src/controllers/car.controller.js
@@ -1,19 +1,46 @@
 import mongoose from "mongoose";
-import {Car} from "../models/car.model.js"
+import { Car } from "../models/car.model.js";
 
-const addCar = async (req, res) =>{
-    try {
-        
-    } catch (error) {
-        res.status(400).json({message: "Error adding car."})
-    }
-}
-const getAllCar = async (req, res) =>{
-    try {
-        
-    } catch (error) {
-        res.status(400).json({message: "Error fetching car list."})
-    }
-}
+const addCar = async (req, res) => {
+  try {
+    const {
+      carName,
+      driverName,
+      driverPhoneNumber,
+      driverPhoneNumberAlt,
+      carType,
+      noOPfSeats,
+      drivingLocation,
+      ac,
+      condition,
+      carRating,
+    } = req.body;
 
-export {addCar, getAllCar}
+    const newCar = new Car({
+      carName,
+      driverName,
+      driverPhoneNumber,
+      driverPhoneNumberAlt,
+      carType,
+      noOPfSeats,
+      drivingLocation,
+      ac,
+      condition,
+      carRating,
+    });
+
+    await newCar.save();
+    
+    res.status(201).json({ message: "Car Added" });
+  } catch (error) {
+    res.status(400).json({ message: "Error adding car." });
+  }
+};
+const getAllCar = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(400).json({ message: "Error fetching car list." });
+  }
+};
+
+export { addCar, getAllCar };
