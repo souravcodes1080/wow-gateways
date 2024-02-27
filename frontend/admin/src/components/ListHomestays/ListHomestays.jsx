8a3457f130fde19e46e49751a024ce40ff43a40b
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-02-27 13:10:26 +0530

    paginate

--- frontend/admin/src/components/ListHomestays/ListHomestays.jsx
+++ frontend/admin/src/components/ListHomestays/ListHomestays.jsx
@@ -5,16 +5,17 @@ import "./listHomestays.css";
 import Sidebar from "../Sidebar/Sidebar";
 import { FaEdit, FaHome, FaPlus, FaRupeeSign, FaTable } from "react-icons/fa";
 import ReactPaginate from "react-paginate";
-
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function ListHomestays() {
   const [homestays, setHomestays] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 3;
 
   const navigate = useNavigate();
+
   useEffect(() => {
     if (!localStorage.getItem("adminAuthorizationToken")) {
       navigate("/admin/login");
@@ -25,17 +26,9 @@ function ListHomestays() {
         const response = await axios.get("http://localhost:8080/homestay");
         setHomestays(response.data);
         console.log(response.data);
-
-        // toast.success("Homestay Listed successfully!", {
-        //   onClose: () => {
-        //   },
-        //   autoClose: 5000,
-        // });
-
       } catch (error) {
-
-        toast.error("Fetch Unsuccessfull!", {
-          className: 'custom-toast-success',
+        toast.error("Fetch Unsuccessful!", {
+          className: "custom-toast-success",
           autoClose: 5000,
         });
         console.log(error);
@@ -49,31 +42,38 @@ function ListHomestays() {
     navigate(`/admin/edithomestay/${id}`);
   };
 
-  //==============================  REACT PAGENATION   ====================
-
-  const [itemOffset, setItemOffset] = useState(homestays.length > 0 ? 0 : undefined);
-  const itemsPerPage = 3;
-
   const endOffset = itemOffset + itemsPerPage;
-
-  const currentItems = homestays.slice(itemOffset, endOffset).reverse();
-
+  const currentItems = homestays
+    .slice(
+      Math.max(homestays.length - endOffset, 0),
+      homestays.length - itemOffset
+    )
+    .reverse();
   const pageCount = Math.ceil(homestays.length / itemsPerPage);
 
   const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % homestays.length;
+    const newOffset = event.selected * itemsPerPage;
     setItemOffset(newOffset);
   };
-
-  //==============================  REACT PAGENATION   ====================
 
   return (
     <div className="admin-panel-wrapper admin-panel-wrapper-add-homestay">
       <div className="dashboard-main-add-homestay">
         <div className="list-product">
           <div className="manage-homestay-header">
-            <h5> <FaHome /> Manage Homestays</h5>
-            <button onClick={() => { navigate("/admin/addhomestay") }}> <FaPlus />Add New Homestay </button>
+            <h5>
+              {" "}
+              <FaHome /> Manage Homestays
+            </h5>
+            <button
+              onClick={() => {
+                navigate("/admin/addhomestay");
+              }}
+            >
+              {" "}
+              <FaPlus />
+              Add New Homestay{" "}
+            </button>
           </div>
           <br />
           <table className="list-product-table" style={{ width: "100%" }}>
@@ -89,7 +89,7 @@ function ListHomestays() {
               </tr>
             </thead>
             <tbody>
-              {homestays.map((homestay, index) => (
+              {currentItems.map((homestay, index) => (
                 <tr key={index}>
                   <td>{homestay.homestayName}</td>
                   <td>{homestay.address}</td>
@@ -115,7 +115,7 @@ function ListHomestays() {
             breakLabel="..."
             nextLabel=">"
             onPageChange={handlePageClick}
-            pageRangeDisplayed={2}
+            pageRangeDisplayed={5}
             pageCount={pageCount}
             previousLabel="<"
             renderOnZeroPageCount={null}
@@ -125,54 +125,6 @@ function ListHomestays() {
       </div>
     </div>
   );
-  
 }
 
 export default ListHomestays;
-{
-  /* <div className="list-product-format-main">
-<p>Picture</p>
-<p>Homestay Name</p>
-<p>Address</p>
-<p>Phone Number</p>
-<p>Email</p>
-<p>Price / day</p>
-<p>Rooms</p>
-<p>Cars</p>
-</div>
-<div className="list-product-all-products">
-<hr />
-{homestays.map((homestay, index) => {
-  return (
-    <div key={index}>
-      <div
-        
-        className="list-product-format-main list-product-format"
-      >
-        <img
-          className="list-product-product-icon"
-          src={homestay.images[4]}
-          alt={homestay.images[4]}
-        />
-        <p>{homestay.homestayName}</p>
-        <p>{homestay.address}</p>
-        <p>{homestay.phoneNumber}</p>
-        <p>{homestay.email}</p>
-        <p>Rs. {homestay.price}</p>
-        <p>{homestay.noOfrooms}</p>
-        <p>{homestay.noOfCars}</p>
-          <button
-          onClick={()=>{updateHomestay(homestay._id)}}
-            className="list-product-update-item"
-          >Update</button>
-         <button
-          onClick={()=>{updateHomestay(homestay._id)}}
-            className="list-product-delete-item"
-          >Delete</button> 
-      </div>
-      <hr />
-    </div>
-  );
-})}
-</div> */
-}
