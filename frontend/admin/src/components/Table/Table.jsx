6e3b127b3e29e5e5061d181896de117e923e28ee
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-03-04 13:31:35 +0530

    changes

--- frontend/admin/src/components/Table/Table.jsx
+++ frontend/admin/src/components/Table/Table.jsx
@@ -1,23 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import "./table.css";
-
 function MonthSection({ month, rooms, roomAvailabilityData }) {
   const daysInMonth = moment().month(month).daysInMonth();
 
   return (
     <>   
-      <p style={{fontSize: "18px", marginTop:"30px"}}>{month}</p>
+      <p style={{ fontSize: "18px", marginTop: "30px" }}>{month}</p>
       <div className="grid-box-wrapper">
         {Array.from({ length: daysInMonth }).map((_, dayIndex) => (
           <div key={dayIndex} className="day-row">
-            <p style={{textAlign: "center"}}>{dayIndex + 1}</p>
+            <p style={{ textAlign: "center" }}>{dayIndex + 1}</p>
             {Array.from({ length: rooms }).map((_, roomIndex) => {
               const isBooked = roomAvailabilityData.some(booking => {
-                const checkIn = moment(booking.checkIn).format('YYYY-MM-DD');
-                const checkOut = moment(booking.checkOut).format('YYYY-MM-DD');
-                const currentDate = moment().month(month).date(dayIndex + 1).format('YYYY-MM-DD');
-                return currentDate >= checkIn && currentDate <= checkOut && booking.rooms.includes(roomIndex + 1);
+                const checkIn = moment(booking.checkIn).date(dayIndex + 1).format('YYYY-MM-DD');
+                const checkOut = moment(booking.checkOut).date(dayIndex + 1).format('YYYY-MM-DD');
+                return checkIn === checkOut && booking.rooms.includes(roomIndex + 1);
               });
               return (
                 <div
@@ -34,6 +32,9 @@ function MonthSection({ month, rooms, roomAvailabilityData }) {
     </>
   );
 }
+
+
+
 
 function Table({ homestayName, rooms }) {
   const months = moment.months();
